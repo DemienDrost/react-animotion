a4d79d9af210aad5f1219fb8afacb28fd520b004
Vincent Beekhuis <v.beekhuis@cycloon.eu>
2023-10-18 19:14:34 +0200

    removed interpolate from useAnimotionHelpers

--- src/hooks/useAnimotionHelpers.ts
+++ src/hooks/useAnimotionHelpers.ts
@@ -1,38 +1,24 @@
 export default function useAnimotionHelpers() {
-  const interpolate = (
-    rangeStart: number,
-    rangeEnd: number,
-    selectedStart: number,
-    selectedEnd: number,
-    value: number
-  ) => {
-    const rangeDiff = rangeEnd - rangeStart;
-    const selectedDiff = selectedEnd - selectedStart;
-    const valueDiff = value - rangeStart;
-    const scale = valueDiff / rangeDiff;
-    return selectedStart + scale * selectedDiff;
-  };
-
   const getStyle = (property: string, value: number | string) => {
     let newProperty: string;
     let newValue: string | number;
     switch (property) {
-      case "y":
-        newProperty = "transform";
+      case 'y':
+        newProperty = 'transform';
         newValue = `translateY(${value}px)`;
         break;
-      case "opacity":
+      case 'opacity':
         newProperty = property;
         newValue = value;
         break;
       default:
         newProperty = property;
-        newValue = value + "px";
+        newValue = value + 'px';
         break;
     }
 
     return [newProperty, newValue];
   };
 
-  return { interpolate, getStyle };
+  return { getStyle };
 }
